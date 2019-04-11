fbd6db97c38e1a24fb16394a9e7f59060f3ddcf6
wolterlw <wolterlentin@gmail.com>
2019-04-11 01:00:01 -0400

    autocommit at 11/04/2019 at 01:00

--- net_modules.py
+++ net_modules.py
@@ -3,153 +3,178 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+def conv3x3(in_planes, out_planes, stride=1, groups=1):
+	"""3x3 convolution with padding"""
+	return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+					 padding=1, groups=groups, bias=False)
+
 
 def conv1x1(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+	"""1x1 convolution"""
+	return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-class ResBlock(nn.Module):
-    expansion = 1
 
-    def __init__(self, n_in, n_out, stride=1, downsample=None):
-        super(ResBlock, self).__init__()
-        self.full = nn.Sequential(
-            conv3x3(n_in, n_out, stride),
-            nn.BatchNorm2d(n_out),
-            nn.ReLU(inplace=True),
-            conv3x3(n_out, n_out),
-            nn.BatchNorm2d(n_out)
-        )
-        self.skip = conv1x1(n_in, n_out)
-        self.downsample = downsample
-        self.stride = stride
-        self.relu = nn.ReLU(inplace=True)
+class BasicBlock(nn.Module):
+	expansion = 1
+	def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, norm_layer=None):
+		super().__init__()
+		if norm_layer is None:
+			norm_layer = nn.BatchNorm2d
+		if groups != 1:
+			raise ValueError('BasicBlock only supports groups=1')
+		# Both self.conv1 and self.downsample layers downsample the input when stride != 1
+		self.conv1 = conv3x3(inplanes, planes, stride)
+		self.bn1 = norm_layer(planes)
+		self.relu = nn.ReLU(inplace=True)
+		self.conv2 = conv3x3(planes, planes)
+		self.bn2 = norm_layer(planes)
+		self.downsample = downsample
+		self.stride = stride
 
-    def forward(self, x):
-        identity = x
-        out_ = self.full(x)
+	def forward(self, x):
+		identity = x
 
-        if self.downsample is not None:
-            identity = self.downsample(x)
-        identity = self.skip(identity)
-        out_ += identity
-        out = self.relu(out_)
+		out = self.conv1(x)
+		out = self.bn1(out)
+		out = self.relu(out)
 
-        return out
+		out = self.conv2(out)
+		out = self.bn2(out)
+
+		if self.downsample is not None:
+			identity = self.downsample(x)
+
+		out += identity
+		out = self.relu(out)
+
+		return out
+
+
+class Bottleneck(nn.Module):
+	expansion = 4
+
+	def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, norm_layer=None):
+		super().__init__()
+		if norm_layer is None:
+			norm_layer = nn.BatchNorm2d
+		# Both self.conv2 and self.downsample layers downsample the input when stride != 1
+		self.conv1 = conv1x1(inplanes, planes)
+		self.bn1 = norm_layer(planes)
+		self.conv2 = conv3x3(planes, planes, stride, groups)
+		self.bn2 = norm_layer(planes)
+		self.conv3 = conv1x1(planes, planes * self.expansion)
+		self.bn3 = norm_layer(planes * self.expansion)
+		self.relu = nn.ReLU(inplace=True)
+		self.downsample = downsample
+		self.stride = stride
+
+	def forward(self, x):
+		identity = x
+
+		out = self.conv1(x)
+		out = self.bn1(out)
+		out = self.relu(out)
+
+		out = self.conv2(out)
+		out = self.bn2(out)
+		out = self.relu(out)
+
+		out = self.conv3(out)
+		out = self.bn3(out)
+
+		if self.downsample is not None:
+			identity = self.downsample(x)
+
+		out += identity
+		out = self.relu(out)
+
+		return out
 
 def LinConv(n_in, n_out):
-    return nn.Sequential(
-            nn.Conv2d(n_in, n_out, kernel_size=1, stride=1, bias=False),
-            nn.BatchNorm2d(n_out),
-            nn.ReLU(True)
-        )
+	return nn.Sequential(
+			nn.Conv2d(n_in, n_out, kernel_size=1, stride=1, bias=False),
+			nn.BatchNorm2d(n_out),
+			nn.ReLU(True)
+		)
 
-class SimpleHourglass(nn.Module):
-    def __init__(self, n_in, n_out, n):
-        super(SimpleHourglass, self).__init__()
-        
-        self.upper_branch = nn.Sequential(
-            ResBlock(n_in, 256),
-            # ResBlock(256, 256),
-            ResBlock(256, n_out)
-        )
-        
-        self.lower_branch = nn.Sequential(
-            nn.MaxPool2d(2, stride=2),
-            ResBlock(n_in, 256),
-            # ResBlock(256, 256),
-            ResBlock(256, 256)
-        )
-        
-        if n > 1:
-            self.inter = SimpleHourglass(n_in, n_out, n-1)
-        else:
-            self.inter = ResBlock(256, n_out)
-        self.last = ResBlock(n_out, n_out)
-        self.laster = conv1x1(2*n_out, n_out)
-        
-    def forward(self, x):
-        upper = self.upper_branch(x)
-        lower = self.lower_branch(x)
-        lower = self.inter(lower)
-        lower = self.last(lower)
-        lower = F.interpolate(lower, scale_factor=2)
-        out = torch.cat((lower,upper),dim=1)
-        out = self.laster(out)
-        return out
 
 class Hourglass(nn.Module):
-    def __init__(self, n_in, n_out, n):
-        super(Hourglass, self).__init__()
-        self.upper_branch = nn.Sequential(
-            ResBlock(n_in, 256),
-            ResBlock(256, 256),
-            ResBlock(256, n_out)
-        )
-        
-        self.lower_branch = nn.Sequential(
-            nn.MaxPool2d(2, stride=2),
-            ResBlock(n_in, 256),
-            ResBlock(256, 256),
-            ResBlock(256, 256)
-        )
-        
-        if n > 1:
-            self.inter = Hourglass(256, n_out, n-1)
-        else:
-            self.inter = ResBlock(256, n_out)
-        self.last = ResBlock(n_out, n_out)
-        
-    def forward(self, x):
-        upper = self.upper_branch(x)
-        lower = self.lower_branch(x)
-        lower = self.inter(lower)
-        lower = self.last(lower)
-        lower = F.interpolate(lower, scale_factor=2)
-        return 0.8*upper + 0.2*lower
+	def __init__(self, n_in, n_out, n):
+		super().__init__()
+		self.upper_branch = nn.Sequential(
+			ResBlock(n_in, 256),
+			ResBlock(256, 256),
+			ResBlock(256, n_out)
+		)
+		
+		self.lower_branch = nn.Sequential(
+			nn.MaxPool2d(2, stride=2),
+			ResBlock(n_in, 256),
+			ResBlock(256, 256),
+			ResBlock(256, 256)
+		)
+		
+		if n > 1:
+			self.inter = Hourglass(256, n_out, n-1)
+		else:
+			self.inter = ResBlock(256, n_out)
+		self.last = ResBlock(n_out, n_out)
+		
+	def forward(self, x):
+		upper = self.upper_branch(x)
+		lower = self.lower_branch(x)
+		lower = self.inter(lower)
+		lower = self.last(lower)
+		lower = F.interpolate(lower, scale_factor=2)
+		return 0.8*upper + 0.2*lower
 
 def torchMaxCoords(hmap):
-    idxs = hmap.view(*hmap.shape[:2], -1).max(dim=-1)[1]
-    xs = idxs % 128
-    ys = idxs // 128
-    return torch.stack([ys,xs], dim=-1)
+	idxs = hmap.view(*hmap.shape[:2], -1).max(dim=-1)[1]
+	xs = idxs % 128
+	ys = idxs // 128
+	return torch.stack([ys,xs], dim=-1)
 
 def torchPCK(hmap_pred, hmap_true, threshold=5):
-    coords_true = torchMaxCoords(hmap_true)
-    coords_pred = torchMaxCoords(hmap_pred)
-    diff = coords_pred - coords_true
-    dist = torch.sqrt((diff ** 2).sum(dim=-1).float())
-    return (dist < threshold).view(-1).float().mean().item()
+	coords_true = torchMaxCoords(hmap_true)
+	coords_pred = torchMaxCoords(hmap_pred)
+	diff = coords_pred - coords_true
+	dist = torch.sqrt((diff ** 2).sum(dim=-1).float())
+	return (dist < threshold).view(-1).float().mean().item()
+
+def coordPCK(threshold=5):
+	def f(coords_pred, coords_true):
+		diff = coords_pred - coords_true
+		dist = torch.sqrt((diff ** 2).sum(dim=-1).float())
+		return (dist < threshold).view(-1).float().mean().item()
+	return f
 
 
 class SoftArgmax(nn.Module):
-    def __init__(self, img_shape):
-        super(SoftArgmax, self).__init__()
-        dim = img_shape[0]
-        X,Y = np.meshgrid(
-            range(1, dim+1),
-            range(1, dim+1)
-        )
-        X = torch.from_numpy(X / dim).type(torch.FloatTensor)
-        Y = torch.from_numpy(Y / dim).type(torch.FloatTensor)
-        
-        self.Wx = torch.nn.Parameter(X.view(1,-1))
-        self.Wy = torch.nn.Parameter(Y.view(1,-1))
-        self.d = 1/dim
-        
-    def forward(self, x):
-        sm = F.softmax(
-            x.flatten(start_dim=2), dim=2
-        )
-        coord_x = torch.bmm(self.Wx.expand_as(sm),
-                            sm.transpose(2, 1)) - self.d
-        coord_y = torch.bmm(self.Wy.expand_as(sm),
-                            sm.transpose(2, 1)) - self.d
+	def __init__(self, img_shape):
+		super().__init__()
+		dim = img_shape
+		X,Y = np.meshgrid(
+			range(1, dim+1),
+			range(1, dim+1)
+		)
+		X = torch.from_numpy(X / dim).type(torch.FloatTensor)
+		Y = torch.from_numpy(Y / dim).type(torch.FloatTensor)
+		
+		self.Wx = torch.nn.Parameter(X.view(1,-1))
+		self.Wy = torch.nn.Parameter(Y.view(1,-1))
+		self.d = 1/dim
+		
+	def forward(self, x):
+		sm = F.softmax(
+			x.flatten(start_dim=2), dim=2
+		)
+		coord_x = torch.bmm(self.Wx.expand_as(sm),
+							sm.transpose(2, 1)) - self.d
+		coord_y = torch.bmm(self.Wy.expand_as(sm),
+							sm.transpose(2, 1)) - self.d
 
-        coords = torch.stack([
-            coord_y.view(-1, 1),
-            coord_x.view(-1, 1)],
-            dim = -1)
-        return coords
+		coords = torch.stack([
+			coord_y.view(-1, 1),
+			coord_x.view(-1, 1)],
+			dim = -1)
+		return coords
--- utils.py
+++ utils.py
@@ -4,7 +4,12 @@ from tensorboardX import SummaryWriter
 from collections import deque
 
 class AverageMeter(object):
-    """Computes and stores the average and current value"""
+    """Keeps an average of a subsequence of certain size
+    
+    Attributes:
+        max_len (int): maximal squence length
+    """
+    
     def __init__(self, max_len=50):
         self.vals = deque(maxlen=max_len)
         self.max_len = max_len
@@ -48,3 +53,39 @@ class TBhook():
 
     def close(self):
         self.writer.close()
+
+class DivergenceLoss():
+
+    """Jensen-Shannon Loss class
+    
+    Attributes:
+        reduction (string): mean or sum
+    """
+    
+    eps = 1e-24
+    def __init__(self, reduction='mean'):
+        self.red = reduction
+
+    def _kl(self, p, q):
+        unsummed_kl = p * ((p + eps).log() - (q + eps).log())
+        if self.red == 'mean':
+            kl_vals = unsummed_kl.mean()
+        else:
+            kl_vals = unsummed_kl.sum()
+        return kl_vals
+
+    def _js(self, p, q):
+        m = 0.5 * (p + q)
+        return 0.5 * self._kl(p, m) + 0.5 * self._kl(q, m)
+
+    def __call__(self, y_pred, y_true):
+        """Calculates JS divergence
+        
+        Args:
+            y_pred torch.floatTensor: predicted heatmaps
+            y_true torch.floatTensor: ground truth heatmaps
+        
+        Returns:
+            torch floatTensor: calculated JS divergence
+        """
+        return self._js(y_pred, y_true)
